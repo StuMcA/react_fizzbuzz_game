f3bc6d824a763f98de9c6cbe25644ad862ba455a
StuMcA <stuart.mcara@outlook.com>
2021-05-25 13:31:20 +0100

    lab finished

--- src/containers/FizzBuzz.js
+++ src/containers/FizzBuzz.js
@@ -2,12 +2,13 @@ import {useState, useEffect} from 'react';
 
 const FizzBuzz = () => {
 
-    const [number, setNumber] = useState(0);
+    const [inputNumber, setNumber] = useState(0);
     const [answer, setAnswer] = useState('');
+    const [playerInput, setPlayerInput] = useState('');
 
     useEffect(()=> {
         if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
-            setAnswer("fizzbuz")
+            setAnswer("fizzbuzz")
           } else if (inputNumber % 3 === 0) {
             setAnswer("fizz")
           } else if (inputNumber % 5 === 0) {
@@ -15,16 +16,30 @@ const FizzBuzz = () => {
           } else {
             setAnswer(inputNumber);
           }      
-    })
+    }, [inputNumber])
 
     const handleInc = ()=> {
-        setNumber(number + 1);
+        setNumber(inputNumber + 1);
     }
+
+    
+
+    // const handleFormSubmit = () => {
+    //     if (answer.toLowerCase() === playerInput.toLowerCase()) {
+
+    //     }
+    // }
     
     return (
         <>
-            <h2>{number}</h2>
-            <button value={number} onClick={handleInc}>Next number</button>
+            <h2>{inputNumber}</h2>
+            {/* <button value={inputNumber} onClick={handleInc}>Next number</button> */}
+            <h3>{answer}</h3>
+            <form onSubmit={handleFormSubmit}>
+                <input type="text" placeholder="Enter answer" value={playerInput} onChange={handlePlayerInputChange}/>
+                <input type="submit" value="Submit guess" />
+            </form>
+            
         </>
     );
 }
